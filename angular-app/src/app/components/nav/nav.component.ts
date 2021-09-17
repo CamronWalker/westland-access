93265c8946f7b87824746ff28c77718eb0c8e3dd
CamronWalker <camronwalker@gmail.com>
2021-09-17 16:06:07 -0600

    project number import works

--- angular-app/src/app/components/nav/nav.component.ts
+++ angular-app/src/app/components/nav/nav.component.ts
@@ -4,11 +4,13 @@ import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { AuthService } from 'src/app/services/auth.service';
 import { FormBuilder, FormGroup } from '@angular/forms'
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 
 
-interface Food {
-  projectValue: string;
-  viewValue: string;
+interface Project {
+  projectName: string;
+  projectUsers: [string];
+  projectImageURL?: string;
 }
 
 @Component({
@@ -19,20 +21,39 @@ interface Food {
 export class NavComponent implements OnInit{
   projectNumber!: Observable<string>;
   projectForm!: FormGroup;
+  projectsCollection!: AngularFirestoreCollection<Project>;
+  projectsList$: any;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
+  
 
   constructor(
     private breakpointObserver: BreakpointObserver, 
     public auth: AuthService,
     private fb: FormBuilder, 
+    private afs: AngularFirestore,
     ) {}
 
 ngOnInit() {
+  this.auth.user$.subscribe(user => {
+    this.projectsCollection = this.afs.collection('projects', ref => {
+      return ref.where('projectUsers', 'array-contains', user.uid);
+    });
+
+    this.projectsCollection.valueChanges({ idField: 'id' }).subscribe(proj => {
+      this.projectsList$ = proj;
+      //console.log(this.projectsList$)
+    })
+
+
+  })
+
+
+
   this.projectForm = this.fb.group({
     projectDrop: '',
   });
@@ -42,10 +63,13 @@ ngOnInit() {
   })
 
  }
- 
-    foods: Food[] = [
-      {projectValue: 'steak-0', viewValue: 'Steak'},
-      {projectValue: 'pizza-1', viewValue: 'Pizza'},
-      {projectValue: 'tacos-2', viewValue: 'Tacos'}
-    ];
+
+
+
+    // foods: Food[] = [
+    //   {projectValue: 'steak-0', viewValue: 'Steak'},
+    //   {projectValue: 'pizza-1', viewValue: 'Pizza'},
+    //   {projectValue: 'tacos-2', viewValue: 'Tacos'}
+    // ];
 }
+  
