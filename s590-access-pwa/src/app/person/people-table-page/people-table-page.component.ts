0bf51d16dc35cd587c41856b65d0e11bbde735be
CamronWalker <camronwalker@gmail.com>
2021-10-22 19:05:55 -0600

    working on debounce table search

--- s590-access-pwa/src/app/person/people-table-page/people-table-page.component.ts
+++ s590-access-pwa/src/app/person/people-table-page/people-table-page.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { Firestore, collection, query, orderBy, collectionData, limit } from '@angular/fire/firestore';
-import { FormBuilder } from '@angular/forms';
+import { Firestore, collection, query, orderBy, collectionData, limit, where } from '@angular/fire/firestore';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-people-table-page',
@@ -15,6 +16,7 @@ export class PeopleTablePageComponent implements OnInit {
   displayedColumns = ['id', 'firstName', 'lastName', 'companyName', 'status', 'trade', 'actions']
   peopleData!: MatTableDataSource<any>;
   @ViewChild(MatSort) sort!: MatSort;
+  peopleTableFilter!: FormGroup;
 
   constructor(
     private firestore: Firestore,
@@ -22,13 +24,37 @@ export class PeopleTablePageComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    const peopleRef = collection(this.firestore, 'projects/S590/people')
-    const peopleColQuery = query(peopleRef, orderBy('badgeNum', 'asc'), limit(50))
-
-    collectionData(peopleColQuery, { idField: 'id' }).subscribe(scans => {
-      this.peopleData = new MatTableDataSource(scans)
-      this.peopleData.sort = this.sort
+    this.peopleTableFilter = this.fb.group({
+      peopleFilterValue: '',
     })
+
+    if (this.peopleTableFilter.get('peopleFilterValue')?.value) {
+      
+    } else {
+
+    }
+
+    this.peopleTableFilter.controls['peopleFilterValue'].valueChanges.pipe(debounceTime(1000)).subscribe(form => {
+      console.log(form)
+      if (form == '') {
+        console.log(form)
+        const peopleRef = collection(this.firestore, 'projects/S590/people')
+        const peopleColQuery = query(peopleRef, orderBy('badgeNum', 'asc'), limit(15))
+
+        collectionData(peopleColQuery, { idField: 'id' }).subscribe(scans => {
+          this.peopleData = new MatTableDataSource(scans)
+          this.peopleData.sort = this.sort
+        })
+      } else {
+        const peopleRef = collection(this.firestore, 'projects/S590/people')
+        const peopleColQuery = query(peopleRef, orderBy('badgeNum', 'asc'), where('searchArray', 'array-contains', form), limit(1))
+
+        collectionData(peopleColQuery, { idField: 'id' }).subscribe(scans => {
+          this.peopleData = new MatTableDataSource(scans)
+          this.peopleData.sort = this.sort
+        })
+      }
+    });
 
   }
 
