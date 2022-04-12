824c6064499c382f106607567b4056e5cb7e20c6
CamronWalker <camronwalker@gmail.com>
2022-04-12 10:31:35 -0600

    new person form html built

--- s590-access-pwa/src/app/person/person-new-page/person-new-page.component.ts
+++ s590-access-pwa/src/app/person/person-new-page/person-new-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { CustomSnackBarService } from 'src/app/shared/services/custom-snackbar.service';
 
 @Component({
   selector: 'app-person-new-page',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./person-new-page.component.scss']
 })
 export class PersonNewPageComponent implements OnInit {
+  newPersonForm!: FormGroup;
+  state!: string;
 
-  constructor() { }
+  constructor(
+    private snackbar: CustomSnackBarService,
+    private fb: FormBuilder,
+    private firestore: Firestore
+  ) { }
 
   ngOnInit(): void {
+    this.newPersonForm = this.fb.group({
+      badgeNum: '',
+      firstName: '',
+      lastName: '',
+      companyName: '',
+      tradeName: '',
+      status: '',
+      statusDesc: '',
+    })
   }
 
+
+  changeHandler(e: any) {
+    this.state = e
+  }
 }
